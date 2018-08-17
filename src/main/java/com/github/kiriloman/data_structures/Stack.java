549073085591879917bc5aff2e78b0177a7c42a0
kiriloman <kiriloman1234@hotmail.com>
2018-08-17 17:08:45 +0100

    Create Stack class.

---
+++ src/main/java/com/github/kiriloman/data_structures/Stack.java
@@ -0,0 +1,39 @@
+package com.github.kiriloman.data_structures;
+
+import java.util.EmptyStackException;
+
+//LIFO data structure
+public class Stack<T> {
+
+    private LinkedList<T> stack;
+
+    public Stack() {
+        stack = new LinkedList<>();
+    }
+
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+
+    public T peek() {
+        if (empty()) throw new EmptyStackException();
+        return (T) stack.getLastNode().getValue();
+    }
+
+    public T pop() {
+        Node last = stack.getLastNode();
+        stack.removeLast();
+        return (T) last.getValue();
+    }
+
+    public T push(T element) {
+        stack.add(element);
+        return element;
+    }
+
+    public int search(Object o) {
+        for (int i = 0; i < stack.length; i++) {
+            if (stack.)
+        }
+    }
+}
