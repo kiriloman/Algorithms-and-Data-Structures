221c928fe2359ffb7520eef22930c4867128ddd6
kiriloman <kiriloman1234@hotmail.com>
2018-08-22 16:16:38 +0100

    Add containsKey, containsValue and remove methods.

--- src/main/java/com/github/kiriloman/data_structures/HashMap.java
+++ src/main/java/com/github/kiriloman/data_structures/HashMap.java
@@ -10,17 +10,15 @@ public class HashMap<K, V> {
         NUM_OF_BUCKETS = 32;
         size = 0;
         buckets = new LinkedList[NUM_OF_BUCKETS];
-        initializeBuckets(buckets);
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new LinkedList<Pair<K, V>>();
+        }
     }
 
     HashMap(int numberOfBuckets) {
         NUM_OF_BUCKETS = numberOfBuckets;
         size = 0;
         buckets = new LinkedList[NUM_OF_BUCKETS];
-        initializeBuckets(buckets);
-    }
-
-    private void initializeBuckets(LinkedList[] buckets) {
         for (int i = 0; i < buckets.length; i++) {
             buckets[i] = new LinkedList<Pair<K, V>>();
         }
@@ -51,6 +49,51 @@ public class HashMap<K, V> {
             }
         }
         return null;
+    }
+
+    public boolean containsKey(K key) {
+        int hash = hash(key);
+        for (int i = 0; i < buckets[hash].length; i++) {
+            if (((Pair) buckets[hash].get(i)).getFirst().equals(key))
+                return true;
+        }
+        return false;
+    }
+
+    public boolean containsValue(V value) {
+        for (int i = 0; i < NUM_OF_BUCKETS; i++) {
+            for (int j = 0; j < buckets[i].length; j++) {
+                if (((Pair) buckets[i].get(j)).getSecond().equals(value))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    public void remove(K key) {
+        int hash = hash(key);
+        for (int i = 0; i < buckets[hash].length; i++) {
+            if (((Pair) buckets[hash].get(i)).getFirst().equals(key)) {
+                buckets[hash].remove(i);
+                size--;
+                return;
+            }
+        }
+    }
+
+    public void remove(K key, V value) {
+        int hash = hash(key);
+        for (int i = 0; i < buckets[hash].length; i++) {
+            if (((Pair) buckets[hash].get(i)).getFirst().equals(key) && ((Pair) buckets[hash].get(i)).getSecond().equals(value))) {
+                buckets[hash].remove(i);
+                size--;
+                return;
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     public int size() {
